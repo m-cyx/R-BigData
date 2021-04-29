43ceec13c06afcbc66e4fc14a5a7febd3331fe85
m-cyx <markpixel05@gmail.com>
2021-04-29 12:22:16 +0300

    update

--- lab2.r
+++ lab2.r
@@ -1,15 +1,30 @@
+t <- read.csv("~/R-BigData/phones.csv", encoding="UTF-8", sep=";")
+t1=t[,2:11]
 
-phones <- read.csv("~/R-BigData/phones.csv", encoding="UTF-8", sep=";")
-plot(data.frame(phones))
 
-phone_names <- names(phones)
-people_names <- phones[,"X.U.FEFF."]
-names(phones)
-d<-data.frame()
-d
+#Минимальное по каждому столбцу
+min_a<-c(apply(t,2,min))[2:11]  
+print(min_a)
 
-table(phones)
-phones.f<-factor(phones)
-phones.f
-plot(phones.f)
-apply(phones,2,mean)
+
+#Максимальное по каждому столбцу
+max_a<-c(apply(t,2,max))[2:11]  
+print(max_a)
+
+
+#Среднее по каждому столбцу
+mean<-round(colMeans(t1)) 
+print(mean)
+
+
+#Вывод в порядке убывания
+sort(round(apply(t1, 2, mean))) 
+
+
+# количество людей, отдавших предпочтение >7 и <3 (составить вектор)
+m=function(t1){length(t1[t1>7 | t1<3])} 
+X=(apply(t1, 2, m))
+
+print(X)
+barplot(mean)
+barplot(X)
